96fff02963b46d36497ee277d951480d000583d4
nick.xue15@gmail.com <nick.xue15@gmail.com>
2022-08-01 21:12:50 +0100

    remove debug code

--- Assets/Code/PerspectiveShifter.cs
+++ Assets/Code/PerspectiveShifter.cs
@@ -6,18 +6,6 @@ public class PerspectiveShifter : MonoBehaviour
     [SerializeField] private Transform verticalViewOrigin;
     [SerializeField] private Transform perceiver;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            Shift(Perspective.Horizontal);
-        }
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            Shift(Perspective.Vertical);
-        }
-    }
-
     public void Shift(Perspective perspective)
     {
         var copyFrom = perspective == Perspective.Horizontal
